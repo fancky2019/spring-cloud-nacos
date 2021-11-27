2d80f233a09e3d24e34824a89e3224d5703b9364
fancky <709737588@qq.com>
2021-11-27 22:36:19 +0800

    配置中心搭建

--- service-consumer/src/main/java/com/example/serviceconsumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/example/serviceconsumer/ServiceConsumerApplication.java
@@ -11,6 +11,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients//启用feign。微服务之间调用,服务发现
 public class ServiceConsumerApplication {
 
+    /*
+    nacos的几个概念
+    命名空间(Namespace)
+    命名空间可用于进行不同环境的配置隔离。一般一个环境划分到一个命名空间
+    配置分组(Group)
+    配置分组用于将不同的服务可以归类到同一分组。一般将一个项目的配置分到一组
+    配置集(Data ID)
+    在系统中，一个配置文件通常就是一个配置集。一般微服务的配置就是一个配置集
+     */
+
+    /*
+     1、在程序中 bootstrap.yaml文件中配置配置中心的信息
+     2、将application.yaml中的配置信息在nacos的配置列表配置发布
+        Data ID=applicationName+profile 如：service-consumer-test.yaml
+     */
     public static void main(String[] args) {
         SpringApplication.run(ServiceConsumerApplication.class, args);
     }
--- service-consumer/src/main/java/com/example/serviceconsumer/service/TestOpenFeignService.java
+++ service-consumer/src/main/java/com/example/serviceconsumer/service/TestOpenFeignService.java
@@ -1,8 +1,10 @@
 package com.example.serviceconsumer.service;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 //@FeignClient(name = "feignClientTest", url = "${sbp.ordermigratedbtoolurl}") springboot
 //@FeignClient(value = "server", fallbackFactory = UserServiceFallBackFactory.class)//开启回调
