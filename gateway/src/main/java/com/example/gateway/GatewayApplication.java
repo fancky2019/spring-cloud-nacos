9a2e2ad13dfc338f1f4d054eaab3f984f5bf3658
fancky2019 <709737588@qq.com>
2021-12-01 15:10:57 +0800

    网关负载均衡策略

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
-
+// #nacos将服务服务注册中心，默认采用应用程序名称。如果两个服务名称一样，网关会采用负载均衡。默认轮训 RoundRobinRule
+    //负载均衡策略：（加权）轮训、随机
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
